b4a33d9deb92a173967272c1bf25739f0d9a72d9
Alexandru Stanciu <alexandru.stanciu@gmail.com>
2015-01-30 09:09:19 +0100

    Init and config from object pattern

--- aggregator/__init__.py
+++ aggregator/__init__.py
@@ -1,36 +1,25 @@
 """
 Aggregator intitialization.
 """
-import config
+import config, database, redis
 
-import database, redis
+def init(config_updates=None):
+    """
+    Delayed init to allow config updates.
+    Updates can be passed as param here or set onto `config` upfront.
+    i.e. `config.SETTING = updates.PREFIX_SETTING or updates.SETTING`
+    """
+    if config_updates:
+        config.from_object(config_updates)
 
-def load_config(updates=None):
-	"""
-	Delayed init to allow config updates.
-	Updates can be passed as param here or set onto `config` upfront.
-	i.e. `config.SETTING = updates.PREFIX_SETTING or updates.SETTING`
-	"""
-	def update_config():
-		"Update same name (or prefixed) settings in config."
-		prefix = config.__name__.split('.')[0].upper()
-		keys = [k for k in config.__dict__ if not k.startswith('_')]
-		get_value = lambda c, k: hasattr(c, k) and getattr(c, k) or None
-		for key in keys:
-			prefix_key = '%s_%s' % (prefix, key)
-			value = get_value(updates, prefix_key) or get_value(updates, key)
-			if value: setattr(config, key, value)
-	if updates:
-		update_config()
+    global db, r
 
-	global db, r
+    ## Database
+    database.init(config)
+    db = database.db
 
-	## Database
-	database.load_config(config)
-	db = database.db
+    ## Redis
+    r = redis.StrictRedis(config.REDIS_HOST, config.REDIS_PORT, config.REDIS_DB)
 
-	## Redis
-	r = redis.StrictRedis(config.REDIS_HOST, config.REDIS_PORT, config.REDIS_DB)
-
-	## Models
-	from models import Link, Reader # delayed
+    ## Models
+    from models import Link, Reader # delayed because of `r`
