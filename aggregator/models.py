e59b55137419c482257312f5623d3c026e17cb1e
Alexandru Stanciu <alexandru.stanciu@gmail.com>
2015-03-20 17:43:28 +0100

    Delayed AD to get BASE_UXTIME

--- aggregator/models.py
+++ aggregator/models.py
@@ -13,12 +13,10 @@ from __future__ import division
 import config, keys, scripts
 from . import r
 
-from config import BASE_UXTIME, \
-    FELLOWS_COUNT, NEWS_COUNT, MARKS_COUNT, MARKS_LIMIT
 from keys import \
     LINK_MARKERS, READER_MARKS, READER_FELLOWS, READER_EDITION
 
-AD = lambda m: m is not None and m/BASE_UXTIME or None # anno Domini
+AD = lambda m: m is not None and m/config.BASE_UXTIME or None # anno Domini
 
 
 class Link(object):
@@ -65,18 +63,18 @@ class Reader(object):
         scripts.unmark(keys=[self.id], args=[link_id])
 
     # Getter methods. Pass count 0 to return all
-    def get_marks(self, count=MARKS_COUNT, withscores=False):
+    def get_marks(self, count=config.MARKS_COUNT, withscores=False):
         "Return latest reader marks as links."
         return r.zrange(keys.get(READER_MARKS, self.id),
             0, count-1, desc=True, withscores=withscores)
 
-    def get_fellows(self, count=FELLOWS_COUNT, withscores=False):
+    def get_fellows(self, count=config.FELLOWS_COUNT, withscores=False):
         "Return fellows as readers based on recorded marks."
         # self.set_fellows() # real time
         return r.zrange(keys.get(READER_FELLOWS, self.id), 
             0, count-1, desc=True, withscores=withscores)
 
-    def get_edition(self, count=NEWS_COUNT, withscores=False):
+    def get_edition(self, count=config.NEWS_COUNT, withscores=False):
         "Return news edition as links based on fellows."
         # self.set_edition() # real time
         return r.zrange(keys.get(READER_EDITION, self.id), 
@@ -90,18 +88,18 @@ class Reader(object):
         self.set_edition(moment_min=moment_min, moment_max=moment_max)
 
     def set_fellows(self, 
-        moment_min=None, moment_max=None, marks_count=MARKS_COUNT):
+        moment_min=None, moment_max=None, marks_count=config.MARKS_COUNT):
         "Aggregate fellows based on marks inside moments interval."
         scripts.set_fellows(keys=[self.id], 
             args=[AD(moment_min), AD(moment_max), marks_count])
 
     def set_edition(self,
-        moment_min=None, moment_max=None, fellows_count=FELLOWS_COUNT):
+        moment_min=None, moment_max=None, fellows_count=config.FELLOWS_COUNT):
         "Aggregate edition from moments interval based on fellows."
         scripts.set_edition(keys=[self.id],
             args=[AD(moment_min), AD(moment_max), fellows_count])
 
     # Maintenance methods
-    def rem_marks(self, keep=MARKS_LIMIT):
+    def rem_marks(self, keep=config.MARKS_LIMIT):
         "Remove old marks, keep as many as specified."
         scripts.rem_marks(keys=[self.id], args=[keep])
