71e2e4b948cb4e7c0371af5fcf6c8ed0ae4d1f39
Alexandru Stanciu <alexandru.stanciu@gmail.com>
2015-09-29 21:14:28 +0000

    Moved load to web models.

--- aggregator/mixes.py
+++ aggregator/mixes.py
@@ -1,131 +1,21 @@
 """
 Mixed models: Reader, Link
 """
-from operator import attrgetter
-
-import config
 from models import \
     Link as AggregatorLink, Reader as AggregatorReader
 from database.models import \
     Link as DatabaseLink, Reader as DatabaseReader
-from database.models import Status, TwitterUser
 
 
 class MixedLink(DatabaseLink, AggregatorLink):
     """
     Link mix from database and aggregator.
     """
-    def __init__(self, **kwargs):
-        "Base init."
-        super(MixedLink, self).__init__(**kwargs)
-        self._pickers = None
-
-    @property
-    def pickers(self):
-        "Sorted link readers."
-        pickers = {int(picker_id): picker_moment for \
-            picker_id, picker_moment in self.get_pickers(withscores=True)}
-        readers = MixedReader.query.filter(MixedReader.id.in_(pickers.keys())).all()
-        for reader in readers:
-            reader.moment = pickers[reader.id]
-        readers.sort(key=attrgetter('moment'), reverse=True)
-        self._pickers = readers
-        return self._pickers
+    pass
 
 
 class MixedReader(DatabaseReader, AggregatorReader):
     """
     Reader mix from database and aggregator.
-    Retrieve from database and sort aggregated data (i.e. fellows, edition)
     """
-    def __init__(self, **kwargs):
-        "Base init."
-        super(MixedReader, self).__init__(**kwargs)
-        self.init()
-
-    def init(self):
-        self._picks = None
-        self._fellows = None
-        self._edition = None
-
-    def load(self):
-        "Load details from database."
-        self.init()
-
-        # Fellows
-        fellows = {int(fellow_id): fellow_fellowship for \
-            fellow_id, fellow_fellowship in self.get_fellows(withscores=True)}
-        if fellows:
-            readers = MixedReader.query.filter(MixedReader.id.in_(fellows.keys())).all()
-            for reader in readers:
-                reader.fellowship = fellows[reader.id]
-            readers.sort(key=attrgetter('fellowship'), reverse=True)
-        else:
-            readers = []
-        self._fellows = readers
-        fellows = self._fellows
-        fellows_dict = {f.id: f for f in fellows}
-
-        # Picks
-        picks = {int(link_id): link_moment for \
-            link_id, link_moment in self.get_picks(withscores=True)}
-        links = MixedLink.query.filter(MixedLink.id.in_(picks.keys())).all()
-        for link in links:
-            link.moment = picks[link.id]
-        links.sort(key=attrgetter('moment'), reverse=True)
-        for link in links:
-            pickers_ids = set(int(picker_id) for picker_id in link.get_pickers())
-            link_fellows = [fellows_dict[fid] for \
-                fid in pickers_ids.intersection(fellows_dict)]
-            link_fellows.sort(key=attrgetter('fellowship'), reverse=True)
-            link.fellows = [self] + link_fellows
-            link.statuses = {}
-        self._picks = links
-        picks_dict = {l.id: l for l in links}
-
-        # Edition
-        edition_fellows = self.get_edition_fellows()
-        edition = {int(news_id): (news_relevance, \
-            [int(fellow_id) for fellow_id in edition_fellows[news_id].split(',')]) \
-            for news_id, news_relevance in self.get_edition(withscores=True)}
-        if edition:
-            links = MixedLink.query.filter(MixedLink.id.in_(edition.keys())).all()
-            for link in links:
-                link.relevance, link.fellows_ids = edition[link.id]
-            links.sort(key=attrgetter('relevance'), reverse=True)
-            for link in links:
-                link_fellows = [fellows_dict[fid] for fid in link.fellows_ids]
-                link_fellows.sort(key=attrgetter('fellowship'), reverse=True)
-                link.fellows = link_fellows
-                link.statuses = {}
-        else:
-            links = []
-        self._edition = links
-        edition_dict = {l.id: l for l in links}
-
-        # Statuses
-        link_ids = picks_dict.keys() + edition_dict.keys()
-        user_ids = [self.twitter_user_id] + [f.twitter_user_id for f in fellows]
-        statuses = Status.query.with_entities(
-            Status.link_id, Status.user_id, Status.status_id).filter(
-            Status.link_id.in_(link_ids), Status.user_id.in_(user_ids)).all()
-        for status in statuses:
-            link_id, user_id, status_id = (status[0], status[1], status[2])
-            link = picks_dict.get(link_id, edition_dict.get(link_id, None))
-            link.statuses[user_id] = status_id
-
-    ## Loaded properties
-    @property
-    def picks(self):
-        "Sorted picked links."
-        return self._picks
-
-    @property
-    def fellows(self):
-        "Sorted fellow readers."
-        return self._fellows
-
-    @property
-    def edition(self):
-        "Sorted edition links."
-        return self._edition
+    pass
